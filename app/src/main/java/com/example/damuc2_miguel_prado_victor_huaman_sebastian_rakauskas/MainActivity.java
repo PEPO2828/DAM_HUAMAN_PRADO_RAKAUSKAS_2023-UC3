4dd064891fbb2eed843f9781d5c007b4199a092e
Prado Zuta, Miguel Angel <mikk_pr@hotmail.com>
2023-05-15 16:13:23 -0500

    Primera Parte UC2 - Prado Zuta

---
+++ app/src/main/java/com/example/damuc2_miguel_prado_victor_huaman_sebastian_rakauskas/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.damuc2_miguel_prado_victor_huaman_sebastian_rakauskas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
