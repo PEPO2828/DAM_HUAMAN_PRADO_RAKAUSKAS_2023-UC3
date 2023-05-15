bdb1f40cee9ab9c3d9bfbf74d0f0a3ddb69ca490
Huaman simeon <victorhuamansimeon@gmail.com>
2023-05-15 17:17:10 -0500

    Tercera parte - Huaman

--- app/src/main/java/com/example/damuc2_miguel_prado_victor_huaman_sebastian_rakauskas/MainActivity.java
+++ app/src/main/java/com/example/damuc2_miguel_prado_victor_huaman_sebastian_rakauskas/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.damuc2_miguel_prado_victor_huaman_sebastian_rakauskas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn1 = findViewById(R.id.button_primero);
+        btn1.setOnClickListener(view -> Toast.makeText(this, "Se envio sus datos", Toast.LENGTH_SHORT).show());
+
     }
 }
